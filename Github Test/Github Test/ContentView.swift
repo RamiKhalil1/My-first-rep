691b9c920a1a766d79c74ee4331a74405e03b70b
Rami Khalil <142479561+RamiKhalil1@users.noreply.github.com>
2024-04-22 13:05:10 +1000

    wekufrgw

--- Github Test/Github Test/ContentView.swift
+++ Github Test/Github Test/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Lets on github")
             Text("New text")
+            Text("newtext")
         }
         .padding()
     }
