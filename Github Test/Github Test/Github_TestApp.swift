163fa15e63f8660bc00b937fb36a47686e735b1a
Rami Khalil <142479561+RamiKhalil1@users.noreply.github.com>
2024-04-22 12:45:56 +1000

    Creating app on XCode

---
+++ Github Test/Github Test/Github_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_TestApp.swift
+//  Github Test
+//
+//  Created by Rami Khalil on 22/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct Github_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
